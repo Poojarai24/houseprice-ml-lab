fc01b210ae926cd447fcc6aeddc740de0c9e5e01
Pooja <raipooja1607@gmail.com>
2025-09-11 13:04:17 +0530

    Created model for house price prediction

--- app/app.py
+++ app/app.py
@@ -4,23 +4,30 @@ import pandas as pd
 
 st.title("🏠 House Price Predictor")
 
-model = joblib.load("model.pkl")
+model = joblib.load("../model.pkl")
 
 with st.form("predict"):
-    rent = st.number_input("Monthly Rent", value=15000)
-    sqft = st.number_input("Square Feet", value=1000)
-    locality = st.text_input("Locality (e.g., Downtown)", "Downtown")
-    bedrooms = st.number_input("Bedrooms", value=2, min_value=0, max_value=10, step=1)
-    parking = st.text_input("Parking (Open/Covered/Missing)", "Open")
+    area = st.number_input("Square Feet (area)", value=1000)
+    rent=st.number_input("Rent",value=50000)
+    facing = st.text_input("Facing Direction (e.g., East, North, West, etc.)", "East")
+    locality = st.text_input("Locality (e.g., BTM Layout)", "BTM Layout")
+    BHK = st.number_input("Bedrooms (BHK)", value=2, min_value=0, max_value=10, step=1)
+    bathrooms = st.number_input("bathrooms", value=2, min_value=0, max_value=10, step=1)
+    parking = st.text_input("Parking (e.g., Bike, Car, Bike and Car, Missing)", "Bike")
     submitted = st.form_submit_button("Predict")
 
 if submitted:
     X = pd.DataFrame([{
-        "rent": rent,
-        "sqft": sqft,
+        "rent":rent,
+        "area": area,
+        "facing": facing if facing else "Missing",
         "locality": locality if locality else "Missing",
-        "bedrooms": bedrooms,
+        "BHK": BHK,
+        "bathrooms": bathrooms,
         "parking": parking if parking else "Missing"
     }])
+    
     pred = model.predict(X)[0]
     st.success(f"Predicted Price: {pred:,.2f}")
+
+
